f7c1e29fb75e860918925c9ce9513adbd93ca1d7
Jörgen Grahn <grahn+src@snipabacken.se>
2022-01-29 12:02:24 +0100

    config::reference(TC)
    This test is based on what the official Configurator app once
    sent, so it's a better test than the others in that sense.
    
    And indeed at this point it fails: my software cannot produce
    that config.

--- test/config.cc
+++ test/config.cc
@@ -70,7 +70,7 @@ namespace cfg {
 	    const auto end = cfg.encode(v.data());
 
 	    auto p = v.data();
-	    get8 ("--gain 0", p, 0x02);
+	    get8 ("--gain 0    ", p, 0x02);
 	    get8 ("--rate 48kHz", p, 4, 16, 1);
 	    get32("            ", p, 384000);
 	    get8 ("            ", p, 8);
@@ -99,6 +99,69 @@ namespace cfg {
 	    get8 ("pack #1     ", p, 0);
 	    get8 ("pack #2     ", p, 0);
 	    get8 ("pack #3     ", p, 0);
+
+	    assert_eq(p, end);
+	}
+
+	/* A configuration I pushed down to an 1.5.0 AudioMoth with
+	 * the official 1.5.1 configurator AppImage and captured using
+	 * strace(1). I had enabled a bunch of features so it wouldn't
+	 * just be zero octets near the end.
+	 *
+	 * 00 06 16 fb f4 61 02 04 10 01 00 dc 05 00 08 05
+	 * 00 37 00 01 02 3c 00 78 00 64 05 a0 05 00 00 00
+	 * 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00 00
+	 * 00 00 00 00 00 00 25 00 ff ff 00 00 02 00 00 03
+	 *
+	 */
+	void reference(TC)
+	{
+	    Config cfg;
+	    cfg.cyclic = Cyclic {"5+55"};
+	    cfg.periods.add("01:00+60");
+	    cfg.periods.add("23:00+60");
+	    cfg.filter = PassFilter {PassFilter::HIGH, "3.7kHz"};
+	    cfg.nimh_batteries = true;
+	    cfg.save_energy = true;
+	    cfg.elephants = true;
+
+	    assert_true(cfg.valid());
+
+	    std::array<uint8_t, 100> v;
+	    v.fill(0xaa);
+	    const auto end = cfg.encode(v.data());
+
+	    auto p = v.data();
+	    get8 ("--gain 0     ", p, 2);
+	    get8 ("--rate 48kHz ", p, 4, 16, 1);
+	    get32("             ", p, 384000);
+	    get8 ("             ", p, 8);
+	    get16("--cyclic 5+55", p, 5, 55);
+
+	    get8 ("--no-rw-led  ", p, 1); // enableLED
+
+	    get8 ("-t           ", p, 2);
+	    get16("  1:00-2:00  ", p, 60, 120);
+	    get16(" 23:00-24:00 ", p, 1380, 1440);
+	    get16("             ", p, 0, 0);
+	    get16("             ", p, 0, 0);
+	    get16("             ", p, 0, 0);
+
+	    get8 ("TZ (hours)  ", p, 0);
+	    get8 ("--ignore-low", p, 0); // enableLowVoltageCutoff
+	    get8 ("--no-battery", p, 0);
+	    get8 ("TZ (minutes)", p, 0);
+
+	    get8 ("--cyclic    ", p, 0); // disableSleepRecordCycle
+
+	    get32("--dates     ", p, 0, 0);
+	    get16("filter      ", p, 37, 0xffff);
+
+	    get16("threshold A ", p, 0);
+	    get8 ("pack #0     ", p, 2);
+	    get8 ("pack #1     ", p, 0);
+	    get8 ("pack #2     ", p, 0);
+	    get8 ("pack #3     ", p, 3);
 
 	    assert_eq(p, end);
 	}
